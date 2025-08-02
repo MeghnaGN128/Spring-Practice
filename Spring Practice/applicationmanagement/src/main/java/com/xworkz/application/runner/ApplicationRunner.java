e32fcb4d617ac39a88247783473f86186ddf6617
Meghna G Ninganur <ninganurmeghna@gmail.com>
2025-08-02 11:01:09 +0530

    Delete Spring Practice/applicationmanagement directory

--- Spring Practice/applicationmanagement/src/main/java/com/xworkz/application/runner/ApplicationRunner.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.application.runner;
-
-public class ApplicationRunner {
-    public static void main(String[] args) {
-
-    }
-}
--- Spring Practice/applicationmanagement/src/main/java/com/xworkz/application/service/ApplicationService.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.application.service;
-
-import com.xworkz.application.entity.ApplicationEntity;
-
-public interface ApplicationService {
-    boolean Check(ApplicationEntity applicationEntity);
-}
